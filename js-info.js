b8a4ff8dce97a06fbc9ecaf236f6ff9e07013740
01305 <114302472+01305@users.noreply.github.com>
2022-10-14 20:31:06 +0300

    first commit

---
+++ js-info.js
@@ -0,0 +1,5 @@
+alert('Кто разработал язык программирования JavaScript?');
+alert('За сколько дней был разработан JavaScript?');
+alert('В каком году появился JavaScript?');
+alert('JavaScript - это то же самое, что и Java? (отвечай либо “JavaScript - это Java”, либо “JavaScript - это не Java”)');
+alert('Как называется спецификация JavaScript?');
